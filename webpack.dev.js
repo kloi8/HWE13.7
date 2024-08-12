4973b4ab3b8769c4b72c4f0d448367f066f286fa
kloi8 <152299798+kloi8@users.noreply.github.com>
2024-08-12 20:36:46 +0300

    update

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,8 +1,8 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config.js');
-const { config } = require('webpack');
+// const { config } = require('webpack');
 
-module.exports = merge(config, {
+module.exports = merge(common, {
     mode: 'development',
     devtool: 'inline-source-map',
     devServer: {
